0418014b9afb36a639c9f4e45e66d56023cc71b7
EamaniManasa <147276480+EamaniManasa@users.noreply.github.com>
2023-10-10 23:44:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,51 @@
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Define the API endpoint
+    const apiUrl = 'https://randomuser.me/api/?results=5';
+
+    // Make an HTTP GET request using Axios
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setUsers(response.data.results);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div className="App">
+      <h1>Axios Example 2</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.login.uuid}>
+            <img src={user.picture.medium} alt="User" />
+            <p>Name: {user.name.first} {user.name.last}</p>
+            <p>Email: {user.email}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
